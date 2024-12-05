2bef840705b68a5018c2fa6528f40d6b5b4545cc
kyeongbobak <kyeong0914@gmail.com>
2024-12-05 10:59:16 +0900

    완전 복잡한 타입 분석하기(flat 편)

--- first.ts
+++ first.ts
@@ -839,11 +839,11 @@ type Name = Profile["name"];
 //   age: 29,
 // };
 
-interface Profile {
-  name: string;
-  age: number;
-  married: boolean;
-}
+// interface Profile {
+//   name: string;
+//   age: number;
+//   married: boolean;
+// }
 
 // type Name = Profile["name"];
 
@@ -870,26 +870,201 @@ interface Profile {
 //   age: 29,
 // };
 
-interface Profile {
-  name: string;
-  age: number;
-  married: boolean;
-}
+// interface Profile {
+//   name: string;
+//   age: number;
+//   married: boolean;
+// }
 
-const zerocho: Profile = {
-  name: "zerocho",
-  age: 29,
-  married: false,
-};
+// const zerocho: Profile = {
+//   name: "zerocho",
+//   age: 29,
+//   married: false,
+// };
 
-type A = Exclude<keyof Profile, "married">; // 빼고 싶은 키값을 빼는 것
+// type A = Exclude<keyof Profile, "married">; // 빼고 싶은 키값을 빼는 것
 
 // S extends keyof any : S는 string | number | symbol
 // 제한 조건 많이 알아두기
 
-type O<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+// type O<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
 
-const newZeroCho: Pick<Profile, Exclude<keyof Profile, "married">> = {
-  name: "zerocho",
-  age: 29,
+// const newZeroCho: Pick<Profile, Exclude<keyof Profile, "married">> = {
+//   name: "zerocho",
+//   age: 29,
+// };
+
+// const zerocho: Profile = {
+//   name: "zerocho",
+//   age: 29,
+//   married: false,
+// };
+
+// type Animal = "Cat" | "Dog" | "Human";
+// type Exclude<T, U> = T extends U ? never : T;
+// type Extract<T, U> = T extends U ? T : never;
+// type Mammal = Exclude<Animal, "Human">; // "Cat" | "Dog" | never
+// type Human = Extract<Animal, "Cat" | "Dog">; // 필요한것 추출해주는 역할
+
+// type A = Exclude<keyof Profile, "married">;
+
+// type O<T, S extends keyof any> = Pick<T, Exclude<keyof T, S>>;
+// const newZeroCho: Omit<Profile, "married"> = {
+//   name: "zerocho",
+//   age: 29,
+// };
+
+// interface Profile {
+//   name?: string;
+//   age?: number;
+//   married?: boolean;
+// }
+
+// type Name = Profile["name"];
+
+// type R<T> = {
+//   [key in keyof T]-?: T[key]; // 옵셔널이였던 값들 모두 옵셔널 떼고 가져오라는 의미가 바로 -?
+// };
+
+// interface 키 값을 모두 필수로 만들고 싶을 때, Required 사용
+// const zerocho: R<Profile> = {
+//   name: "zerocho",
+//   age: 29,
+//   married: false,
+// };
+
+// interface Obj {
+//   [key: string]: number;
+// }
+
+// type R<T extends keyof any, S> = {
+//   [key in T]: S;
+// };
+
+// const a: Obj = { a: 3, b: 5, c: 7 };
+
+// const a: Record<symbol, number> = { a: 3, b: 5, c: 7 };
+
+// interface Obj {
+//   [key: string]: number;
+// }
+
+// 위 코드를 간단하게 만들어둔 것이
+
+// Record<string, number>
+
+// type A = string | null | boolean | number;
+
+// type B = NonNullable<A>; // 위 A라는 Type에서 null과 undefined를 빼고 가져오고 싶을 때, NonNullable을 쓴다
+
+// Pick, ReadOnly, Record, Partial interface에 적용되는 것들
+// Exclude, Extract, NonNullable 등은 key에 적용되는 것들
+
+// type B = N<A>;
+
+// type N<T> = T extends null | undefined ? never : T; // string | boolean | number
+
+// function zip(x: number, y: string, z: boolean): { x: number; y: string; z: boolean } {
+//   return { x, y, z };
+// }
+
+// type P<T extends (...args: any) => any> = T extends (...args: infer A) => any ? A : never; // 추론
+
+// 위 z 함수의 return 타입을 가지고 오고 싶다.
+
+// type R<T extends (...args: any) => any> = T extends (...args: any) => infer A ? A : never; // 추론,
+
+// type Params = Parameters<typeof zip>; // 자동으로 읽어올 수 있게끔 할 수 있다.
+// type Ret = ReturnType<typeof zip>;
+// type First = Params[0];
+
+// type ConstructorParameters<T extends abstract new (...args : any) => any> = T extends abstract new (...args : infer)
+
+// type InstanceType<T extends abstract new (...args : any) => any> = T extends abstract new (...args : any) => infer R? R : any; // 똑같은 모양을 만들어냄으로서 그 자리에 타입을 찾아내는 infer
+
+// class A {
+//   a: string;
+//   b: number;
+//   c: boolean;
+
+//   constructor(a: string, b: number, c: boolean) {
+//     this.a = a;
+//     this.b = b;
+//     this.c = c;
+//   }
+// }
+
+// const c = new A("123", 456, true); // 인스턴스
+// type C = ConstructorParameters<typeof A>; // typeof 클래스가 생성자
+// type I = InstanceType<typeof A>;
+
+const p1 = Promise.resolve(1)
+  // Promise<number>, Promise<number>, Promise<number>, Promise<string>
+  .then((a) => a + 1)
+  .then((a) => a + 1)
+  .then((a) => a.toString());
+
+const p2 = Promise.resolve(2); // Promise<number>
+
+const p3 = new Promise((res, rej) => {
+  // Promise<unknown>
+  setTimeout(res, 1000);
+});
+
+Promise.all([p1, p2, p3]).then((result) => {
+  // {"0" : string, "1" : number, "2 : unknown"}
+  console.log(result); // ["3", 2, undefined]
+});
+
+// T = [p1, p2, p3] {"0" : p1, "1" : p2, "2" : p3, length : 3}
+// keyof T = "0" | "1" | "2" | "length"
+
+// Awaited은 Promise 중첩된 부분을 풀어주는 것
+
+// type Result = Awaited<{ then: (v: number) => number }>; // thenable
+// 이 코드를
+
+// function a(this: Window | typeof obj) {
+//   console.log(this.name);
+// }
+
+// const obj = { name: "zerocho" };
+// const b = a.bind(obj);
+// b(); // "zerocho";
+
+// type T = ThisParameterType<typeof a>;
+
+// bind를 통해서 this를 바꿔주는 예시
+// const zerocho = {
+//   name: "zeorcho",
+//   sayHello() {
+//     console.log(`hi ${this.name}`);
+//   },
+// };
+
+// const sayHello = zerocho.sayHello;
+// const sayHi = zerocho.sayHello.bind({ name: "nero" });
+
+// function add(a: number, b: number, c: number, d: number, e: number, f: number) {
+//   return a + b + c + d + e + f;
+// }
+
+// const add1 = add.bind(null);
+// add1(1, 2, 3, 4, 5, 6);
+
+// const add2 = add.bind(null, 1);
+// add2(2, 3, 4, 5, 6);
+
+// const add3 = add.bind(null, 1, 2); // 이것은 매개변수 각각의 자리에 1,2라는 숫자를 가져다 놓는것이 bind 매개변수의 역할,
+// add3(3, 4, 5, 6); // 그리고 인수로 넘길 때도 지정된 매개변수는 제외한 인수를 보내야 함
+
+const a = [1, 2, 3, [1, 2], [[1], [2]]].flat();
+
+type A = {
+  name: string;
+  age: number;
 };
+
+type B = A["1" extends number ? "age" : "name"];
+
+// type 적으로 1을 빼주는게 어떻게 빼줘야할까 그게 너무 궁금했거든요
